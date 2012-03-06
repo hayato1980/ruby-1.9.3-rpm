84078cdb6c6074f75befd896ab5997ed4659b64a
Hayato ITO <haya10.ito@gmail.com>
2012-03-06 22:22:56 +0900

    delete yum install and check required packages

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-yum install -y rpm-build rpmdevtools readline-devel ncurses-devel gdbm-devel tcl-devel openssl-devel db4-devel byacc
+rpm -q rpm-build rpmdevtools readline-devel ncurses-devel gdbm-devel tcl-devel openssl-devel db4-devel byacc
+
+if [ $? -ne 0 ]; then
+  echo "Some required packages is not installed."
+  exit 1
+fi
 
 HOME=$(cd $(dirname $0) && pwd)
 
@@ -8,5 +13,5 @@ rpmdev-setuptree
 cd ${HOME}/rpmbuild/SOURCES
 wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p125.tar.gz
 cd ${HOME}/rpmbuild/SPECS
-wget https://raw.github.com/imeyer/ruby-1.9.3-rpm/master/ruby19.spec
+cp ../../ruby-19.spec
 rpmbuild -bb ruby19.spec
